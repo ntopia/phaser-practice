fbb2da86a96519f5507c0d0e9616596b57218d02
ntopia <ntopia.p@gmail.com>
2016-04-24 20:41:00 +0900

    연습

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 
 window.onload = function () {
-    var game = new Phaser.Game('100', '100', Phaser.AUTO, 'screen', { preload: preload, create: create });
+    var game = new Phaser.Game('100', '100', Phaser.AUTO, 'screen',
+        { preload: preload, create: create, update: update }
+    );
 
-    function preload () {
+    function preload() {
         game.load.image('logo', 'Original_Doge_meme.jpg');
     }
 
-    function create () {
+    function create() {
         var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
         logo.anchor.setTo(0.5, 0.5);
+        logo.width = 500;
+        logo.height = 500;
+    }
+
+    function update() {
+        
     }
 };
