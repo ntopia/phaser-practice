e13f5f042c084a1df6ff20497ee932b8b60aa460
ntopia <ntopia.p@gmail.com>
2016-04-24 21:09:09 +0900

    브라우저 크기를 조절해도 풀스크린 제대로 되도록 고침

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 
 window.onload = function () {
-    var game = new Phaser.Game('100', '100', Phaser.AUTO, 'screen',
+    var game = new Phaser.Game('100', '100', Phaser.AUTO, '',
         { preload: preload, create: create, update: update }
     );
 
     function preload() {
+        game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+
         game.load.image('logo', 'Original_Doge_meme.jpg');
     }
 
+    var logo;
     function create() {
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
         logo.anchor.setTo(0.5, 0.5);
         logo.width = 500;
         logo.height = 500;
     }
 
     function update() {
-        
+        logo.x = game.world.centerX;
+        logo.y = game.world.centerY;
     }
 };
